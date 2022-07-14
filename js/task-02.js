15ca5b5f8cf8981ffe3b1fb5ce3299305416f2cd
Kateryna Pliusnina <97609067+KatePls@users.noreply.github.com>
2022-07-14 23:06:25 +0300

    task-2 done
    task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ul = document.querySelector('#ingredients');
+console.log(ul);
+for (const elem of ingredients) {
+  let li = document.createElement("li");
+  li.classList.add('item');
+  li.textContent = elem;
+  ul.append(li);
+}
