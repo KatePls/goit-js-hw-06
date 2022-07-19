007d7cf99b25080a47a48a30d9f7860d3e98bb32
Kateryna Pliusnina <97609067+KatePls@users.noreply.github.com>
2022-07-19 21:22:31 +0300

    task-08 done
    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formElem = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+    event.preventDefault();
+ const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    } else {
+        const formElements = {
+            email, password
+        };
+        formElements.email = email.value;
+        formElements.password = password.value;
+        console.log(formElements);      
+    }
+  event.currentTarget.reset();
+}
+
+formElem.addEventListener("submit", handleSubmit);
