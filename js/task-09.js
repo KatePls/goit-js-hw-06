a7bc00d1ef5b6713062413515fa9340b392a3911
Kateryna Pliusnina <97609067+KatePls@users.noreply.github.com>
2022-07-19 21:42:32 +0300

    task-09 done
    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyElem = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+
+function changeColor() {
+  textColor.textContent = getRandomHexColor();
+  bodyElem.style.backgroundColor = getRandomHexColor();
+}
+
+btn.addEventListener('click', changeColor);
