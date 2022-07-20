4bd7ee3c1ef035b9debfec8c91d1dc47aa9f0a26
Kateryna Pliusnina <97609067+KatePls@users.noreply.github.com>
2022-07-20 16:03:10 +0300

    task-10 done
    task-10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputValue = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const divBoxes = document.querySelector("#boxes");
+let amount ;  
+  
+inputValue.addEventListener("input", (event) => {
+ amount = event.currentTarget.value;
+});
+
+function createBoxes() { 
+  for (let i = 0; i < amount; i+=1){
+    let div = document.createElement("div");
+    div.style.width = String(30 + i*10 + "px");
+    div.style.height = String(30 + i*10 + "px");
+    div.style.backgroundColor = getRandomHexColor();
+    div.classList.add('div-item');
+    divBoxes.append(div);
+  }
+}
+
+function destroyBoxes() {
+  const divs = document.querySelectorAll(".div-item");
+  divs.forEach((div) => div.remove());
+}
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
